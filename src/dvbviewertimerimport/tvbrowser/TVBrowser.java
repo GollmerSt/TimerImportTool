c8fe93667c027333f3602b125a3662453ccb3f9f
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2010-05-06 21:44:22 +0000

    git-svn-id: svn://192.168.0.51/Trunk@247 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- src/dvbviewertimerimport/tvbrowser/TVBrowser.java
+++ src/dvbviewertimerimport/tvbrowser/TVBrowser.java
@@ -8,52 +8,56 @@ import dvbviewertimerimport.control.ChannelSet;
 import dvbviewertimerimport.control.Control;
 import dvbviewertimerimport.provider.Provider;
 
-public class TVBrowser extends Provider {
-  
-  
+public class TVBrowser extends Provider
+{
+	boolean isTVBrowserPlugin = false ;
 
-  public TVBrowser(Control control ) {
-    super(control, false, false, "TV-Browser", false, false, false, false, true, false);
-  }
-  @Override
-  public int importChannels( boolean check )
-  {
-    if ( check )
-      return 0 ;
-    
-    String [] channels = DVBViewerTimerImport.getTVBChannelNames() ;
-    
-    int count = 0 ;
-    
-    HashMap< String, ChannelSet > mapByName = new HashMap< String, ChannelSet >() ;
-    
-    int pid = this.getID() ;
-    
-    for ( ChannelSet cs : this.control.getChannelSets() )
-    {
-      Channel c = cs.getChannel( pid ) ;
-      if ( c == null )
-        continue ;
-      mapByName.put( c.getName(), cs ) ;
-    }
-    
-    for ( String channel : channels )
-    {
-      if ( ! mapByName.containsKey( channel ) )
-      {
-        ChannelSet cs = new ChannelSet() ;
-        cs.add( pid, channel, -1 ) ;
-        control.getChannelSets().add( cs ) ;
-        mapByName.put( channel, cs ) ;
-        count++ ;
-      }
-    }
-    return count ;
-  }
-  private String getParaInfo()
-  {
-    return 
-    ", necessary parameters:\n   -TVGenial TVUID=ccc Beginn=yyyyMMddHHmm Dauer=nnn Sendung=cccccc" ;
-  }
+	public void setIsTVBrowserPlugin() { isTVBrowserPlugin = true ; } ;
 
+	public TVBrowser(Control control )
+	{
+		super(control, false, false, "TV-Browser", false, false, false, false, true, false);
+	}
+	  
+	@Override
+	public int importChannels( boolean check )
+	{
+		if ( check )
+			return 0 ;
+		
+		if ( ! isTVBrowserPlugin )
+			return -1 ;
+    
+		String [] channels = DVBViewerTimerImport.getTVBChannelNames() ;
+    
+		if ( channels == null )
+			return -1 ;
+    
+		int count = 0 ;
+    
+		HashMap< String, ChannelSet > mapByName = new HashMap< String, ChannelSet >() ;
+    
+		int pid = this.getID() ;
+    
+		for ( ChannelSet cs : this.control.getChannelSets() )
+		{
+			Channel c = cs.getChannel( pid ) ;
+			if ( c == null )
+				continue ;
+			mapByName.put( c.getName(), cs ) ;
+		}
+    
+		for ( String channel : channels )
+		{
+			if ( ! mapByName.containsKey( channel ) )
+			{
+				ChannelSet cs = new ChannelSet() ;
+				cs.add( pid, channel, -1 ) ;
+				control.getChannelSets().add( cs ) ;
+				mapByName.put( channel, cs ) ;
+				count++ ;
+			}
+		}
+		return count ;
+	}
 }
