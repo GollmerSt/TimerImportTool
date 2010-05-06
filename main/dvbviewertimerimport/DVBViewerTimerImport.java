ffef5dd76c7e7ae5f4f3ad12b13fef4975e92df4
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2010-05-06 21:44:45 +0000

    git-svn-id: svn://192.168.0.51/Trunk@248 a612081f-a2ea-5e49-8e5c-369f9a3f4661

---
+++ main/dvbviewertimerimport/DVBViewerTimerImport.java
@@ -0,0 +1,13 @@
+package dvbviewertimerimport;
+
+/**
+ * @author Stefan Gollmer
+ *
+ */
+public class DVBViewerTimerImport 
+{
+  public static String[] getTVBChannelNames()
+  {
+	  return null ;
+  }
+ }
