a62ff041397ecf4d09f95cc46918fbd13bdd6865
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2010-05-10 05:41:27 +0000

    git-svn-id: svn://192.168.0.51/Trunk@255 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- plugin/dvbviewertimerimport/DVBViewerTimerImport.java
+++ plugin/dvbviewertimerimport/DVBViewerTimerImport.java
@@ -2,6 +2,7 @@ package dvbviewertimerimport;
 
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 import javax.swing.AbstractAction;
@@ -14,15 +15,18 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import devplugin.ActionMenu;
+import devplugin.Date;
 import devplugin.Plugin;
 import devplugin.PluginInfo;
 import devplugin.Program;
 import devplugin.SettingsTab;
 import devplugin.ThemeIcon;
 import devplugin.Version;
+import dvbviewertimerimport.control.Channel;
 import dvbviewertimerimport.control.ChannelSet;
 import dvbviewertimerimport.control.Control;
 import dvbviewertimerimport.dvbviewer.DVBViewer;
+import dvbviewertimerimport.dvbviewer.DVBViewerProvider;
 import dvbviewertimerimport.dvbviewer.channels.Channels;
 import dvbviewertimerimport.gui.GUI;
 import dvbviewertimerimport.gui.GUIPanel;
@@ -30,6 +34,7 @@ import dvbviewertimerimport.gui.WorkPathSelector;
 import dvbviewertimerimport.gui.GUI.GUIStatus;
 import dvbviewertimerimport.main.TimerImportTool;
 import dvbviewertimerimport.main.Versions;
+import dvbviewertimerimport.misc.Constants;
 import dvbviewertimerimport.misc.ErrorClass;
 import dvbviewertimerimport.misc.Log;
 import dvbviewertimerimport.misc.ResourceManager;
@@ -40,7 +45,7 @@ import dvbviewertimerimport.tvbrowser.TVBrowser;
  * @author Stefan Gollmer
  *
  */
-public class DVBViewerTimerImport extends Plugin
+public class DVBViewerTimerImport extends Plugin implements DVBViewerProvider
 {
   private static Version version = null ;
   static private final String exeName = "TimerImportTool" ;
@@ -52,7 +57,21 @@ public class DVBViewerTimerImport extends Plugin
   private Channels channels = null ;
   private GUIPanel settingsPanel = null ;
   
+  private DVBViewerProvider dvbViewerProvider = this ;
+  private final int providerID ;
+  private final Provider provider ;
+  
+  private final GregorianCalendar calendar ;
+  
   private HashMap< String, String > channelAssignmentDvbVToTvB = null ;
+  
+  public DVBViewerTimerImport()
+  {
+    init() ;
+    this.provider = Provider.getProvider( "TV-Browser" ) ;
+    this.providerID = provider.getID() ;
+    calendar = new GregorianCalendar( this.provider.getTimeZone() ) ;
+  }
   
   public static String[] getTVBChannelNames()
   {
@@ -62,6 +81,8 @@ public class DVBViewerTimerImport extends Plugin
       res.add( c.getName() ) ;
     return res.toArray( new String[0] ) ;
   }
+  
+  
   
   private void init()
   {
@@ -84,10 +105,7 @@ public class DVBViewerTimerImport extends Plugin
         dvbViewer.setPathFileIsUsed( true ) ;
       }
 
-      this.channels = new Channels( dvbViewer ) ;
-      this.channels.read() ;
-
-      control = new Control(dvbViewer) ;
+     control = new Control(dvbViewer) ;
     } catch (ErrorClass e) {
       Log.error(e.getLocalizedMessage());
       Log.out("Import terminated with errors" ) ;
@@ -99,8 +117,6 @@ public class DVBViewerTimerImport extends Plugin
     }
     Provider provider = Provider.getProvider( "TV-Browser" ) ;
     ((TVBrowser)provider).setIsTVBrowserPlugin() ;
-
-    getTvBChannelName( "abc" ) ;
    }
     /*
       //Log.setVerbose( true ) ;
@@ -162,7 +178,6 @@ public class DVBViewerTimerImport extends Plugin
       dvbViewer.updateDVBViewer() ;
   }
 
-  control.setDVBViewerEntries() ;
 
   if ( provider != null )
   {
@@ -187,8 +202,6 @@ public class DVBViewerTimerImport extends Plugin
   e.printStackTrace();
   System.exit(2);
 }
-if ( dvbViewer != null )
-  dvbViewer.writeXML() ;
 if ( ErrorClass.isWarning() )
 {
   Log.out( "Import finished, but warnings occurs. The messages should be checked"  ) ;
@@ -231,13 +244,12 @@ System.exit(0);
   }
   public String getTvBChannelName( String dvbVChannelName )
   {
-    int providerID = Provider.getProviderID( "TV-Browser" ) ;
     if ( channelAssignmentDvbVToTvB == null )
     {
       channelAssignmentDvbVToTvB = new HashMap< String, String >() ;
       for ( ChannelSet cs : this.control.getChannelSets() )
       {
-        dvbviewertimerimport.control.Channel providerChannel = cs.getChannel( providerID ) ;
+        dvbviewertimerimport.control.Channel providerChannel = cs.getChannel( this.providerID ) ;
         String dvbViewerChannel = cs.getDVBViewerChannel() ;
         if ( providerChannel != null && dvbViewerChannel != null )
           channelAssignmentDvbVToTvB.put( cs.getDVBViewerChannel().split("[|]")[0], providerChannel.getName() ) ;
@@ -255,6 +267,15 @@ System.exit(0);
       @Override
       public void actionPerformed(ActionEvent evt)
       {
+        control.setDVBViewerEntries() ;
+        try {
+          control.getDVBViewer().process( dvbViewerProvider, false, program ) ;
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        if ( dvbViewer != null )
+          dvbViewer.writeXML() ;
         markProgram( program ) ;
         program.validateMarking() ;
       }
@@ -285,7 +306,7 @@ System.exit(0);
       init() ;
       if ( settingsPanel == null)
       {
-        settingsPanel = new GUIPanel( control, channels ) ;
+        settingsPanel = new GUIPanel( control ) ;
         settingsPanel.paint() ;
       }
       return settingsPanel ;
@@ -318,5 +339,47 @@ System.exit(0);
   public SettingsTab getSettingsTab()
   {
     return new DVBVSettingsTab() ;
+  }
+
+  @Override
+  public void process(boolean getAll)
+  {
+  }
+
+  @Override
+  public void processEntry(Object arg)
+  {
+    Program program = (Program)arg ;
+    calendar.clear() ;
+    Date d = program.getDate() ;
+    calendar.set(
+        d.getYear(),
+        d.getMonth(),
+        d.getDayOfMonth(),
+        program.getHours(),
+        program.getMinutes(),
+        0 ) ;
+    long start = calendar.getTimeInMillis() ;
+    long length = program.getLength() * 1000 * 60 ;
+    long end = start ;
+    if ( length >= 0 )
+      end += length ;
+    else
+      end += Constants.DAYMILLSEC ;
+    
+    String channel = null ;
+    for ( ChannelSet cs : control.getChannelSets() )
+    {
+      Channel c = cs.getChannel( providerID ) ;
+      if ( c == null )
+        continue ;
+      if ( program.getChannel().getName().equals( c.getName() ) )
+      {
+        channel = c.getName() ;
+        break ;
+      }
+    }
+
+    control.getDVBViewer().addNewEntry( provider, program.getID(), channel, start, end, program.getTitle() ) ;
   }
 }
