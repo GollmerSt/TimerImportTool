048f783373623c6e0ff50960697b529ded5c0302
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2010-05-20 18:47:41 +0000

    git-svn-id: svn://192.168.0.51/Trunk@330 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- src/dvbviewertimerimport/gui/GUI.java
+++ src/dvbviewertimerimport/gui/GUI.java
@@ -4,11 +4,9 @@
 
 package dvbviewertimerimport.gui;
 
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
