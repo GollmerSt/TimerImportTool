a37fdc9fa35143c139ca3a15561c302a55330293
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2010-05-24 12:34:04 +0000

    git-svn-id: svn://192.168.0.51/Trunk@372 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- src/dvbviewertimerimport/main/Versions.java
+++ src/dvbviewertimerimport/main/Versions.java
@@ -7,7 +7,7 @@ package dvbviewertimerimport.main;
 public class Versions {
 	private static final int MAJOR_VERSION					= 1 ;
   private static final int MINOR_VERSION					= 0 ;
-  private static final int SUB_VERSION						= 1 ;
+  private static final int SUB_VERSION						= 2 ;
 	private static final String DVBVIEWER_COM_DLL_VERSION	= "1.00.00" ;
 	
 	public static String getVersion() { return Integer.toString( MAJOR_VERSION ) 
