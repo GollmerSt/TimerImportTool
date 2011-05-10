46c1b1d8171ed658c63f797e8c4e670a6b8b8f62
Stefan <Stefan@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2011-05-10 21:32:15 +0000

    git-svn-id: svn://192.168.0.51/Trunk@525 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- src/dvbviewertimerimport/gui/GUI.java
+++ src/dvbviewertimerimport/gui/GUI.java
@@ -207,7 +207,7 @@ public class GUI extends JFrame{
 		}
 	    this.setDefaultCloseOperation( JFrame.DO_NOTHING_ON_CLOSE );
 	    this.setIconImage( this.programIcon.getImage() ) ;
-	    this.setTitle( dvbviewertimerimport.misc.Constants.PROGRAM_NAME + " " + Versions.getVersion() ) ;
+	    this.setTitle( dvbviewertimerimport.misc.Constants.PROGRAM_NAME + " " + Versions.getVersion( true ) ) ;
 	    this.setLayout(  new GridBagLayout() ) ;
         
 		GridBagConstraints c = null ;
