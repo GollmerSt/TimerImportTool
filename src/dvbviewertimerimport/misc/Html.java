a2ae35eacef9f4f5c7a40811eea52e2d4fce1bfc
Stefan Gollmer <Stefan Gollmer@a612081f-a2ea-5e49-8e5c-369f9a3f4661>
2011-06-03 17:47:54 +0000

    From now cookies are possible
    git-svn-id: svn://192.168.0.51/Trunk@597 a612081f-a2ea-5e49-8e5c-369f9a3f4661

--- src/dvbviewertimerimport/misc/Html.java
+++ src/dvbviewertimerimport/misc/Html.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Html {
 	public static InputStream  getStream( String urlString )
@@ -13,6 +14,11 @@ public class Html {
 
 	public static InputStream  getStream( String urlString, String urlType )
 	{
+		return getStream( urlString, urlType, null ) ;
+	}
+
+	public static InputStream  getStream( String urlString, String urlType, String cookies )
+	{
 		if ( urlType != null )
 			Log.out( true, urlType + " URL: " + urlString ) ;
 		URL url;
@@ -21,13 +27,28 @@ public class Html {
 		} catch (MalformedURLException e) {
 			throw new ErrorClass( e, "The URL \"" + urlString + "\" is malformed, the setup should be checked." ) ;
 		}
-
+		
+		URLConnection conn = null ;
 		InputStream result = null ;
+		
+		try {
+			conn = url.openConnection();
+			if ( cookies != null )
+				conn.setRequestProperty("Cookie", cookies );
+			conn.connect() ;
+			result = conn.getInputStream() ;
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+/*
 		try {
 			result = url.openStream() ;
 		} catch (IOException e) {
 			throw new ErrorClass( e, "The URL \"" + urlString + "\" is not available." );
 		}
+*/
 		return result ;
 	}
 
