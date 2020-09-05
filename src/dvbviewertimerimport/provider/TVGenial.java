82d5e1b506dfc890f0ebc2681172c18f55d65c28
Stefan Gollmer <Stefan.Gollmer57@gmail.com>
2020-09-05 08:54:14 +0200

    Preparing 01.04.01

--- src/dvbviewertimerimport/provider/TVGenial.java
+++ src/dvbviewertimerimport/provider/TVGenial.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.TimeZone;
 
 import dvbviewertimerimport.control.Channel;
-import dvbviewertimerimport.control.ChannelSet;
 import dvbviewertimerimport.control.Control;
 import dvbviewertimerimport.dvbviewer.DVBViewer;
 import dvbviewertimerimport.main.Versions;
